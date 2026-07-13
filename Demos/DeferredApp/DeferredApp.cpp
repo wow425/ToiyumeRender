484aac2376a634125fd04cb27a80c22bdd86e6ac
saber2004 <1286073242@qq.com>
2026-07-13 20:09:09 +0800

    clion test

--- Demos/DeferredApp/DeferredApp.cpp
+++ Demos/DeferredApp/DeferredApp.cpp
@@ -23,7 +23,7 @@ using Renderer::MeshSorter;
 class Tooiyume : public GameCore::IGameApp
 {
 public:
-	Tooiyume(void) {}
+	Tooiyume(void) = default;
 
 	virtual void Startup(void) override;
 	virtual void Cleanup(void) override;
