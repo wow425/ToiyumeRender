c80bba715d6bc271324fc0c2362ad65dad6c8fbb
saber2004 <1286073242@qq.com>
2026-07-17 11:26:47 +0800

    TAA初步建设

---
+++ Engine/04_Renderer/TemporalEffects.cpp
@@ -0,0 +1,9 @@
+﻿//
+// Created by saber on 2026/7/17.
+//
+
+#include "TemporalEffects.h"
+
+namespace TemporalEffects
+{
+} // TemporalEffects
---
+++ Engine/04_Renderer/TemporalEffects.h
@@ -0,0 +1,15 @@
+﻿//
+// Created by saber on 2026/7/17.
+//
+
+#ifndef FOLDER_TEMPORALEFFECTS_H
+#define FOLDER_TEMPORALEFFECTS_H
+
+namespace TemporalEffects
+{
+    class TemporalEffects
+    {
+    };
+} // TemporalEffects
+
+#endif //FOLDER_TEMPORALEFFECTS_H
